79e4350f5659d587cc79486a6a76ae0a82d9e22b
Madelyn Petersohn <mcs526@mail.missouri.edu>
2015-11-20 01:37:32 -0600

    We have added files

---
+++ main.js
@@ -0,0 +1,98 @@
+TweenMax.to(".snow1", 10, {
+	y:320,
+	rotation:360,
+	scale:1.2
+});
+
+TweenMax.to(".snow2", 10, {
+	y:320,
+	rotation:120,
+	scale:0.6
+});
+
+TweenMax.to(".snow3", 10, {
+	y:320,
+	rotation:280,
+	scale:1.4
+});
+
+TweenMax.to(".snow4", 3, {
+	y:320,
+	rotation:180,
+	scale:0.8
+});
+
+TweenMax.to(".snow5", 10, {
+	y:320,
+	rotation:220,
+	scale:0.7
+});
+
+TweenMax.to(".snow6", 3, {
+	y:320,
+	rotation:320,
+	scale:1.3
+});
+
+TweenMax.to(".glow1", 5, {
+	delay:0.3,
+	scale:1.5,
+	ease:RoughEase.ease
+});
+
+TweenMax.to(".glow2", 5, {
+	delay:0.4,
+	scale:1.5,	
+	ease:RoughEase.ease
+});
+
+TweenMax.to(".glow3", 5, {
+	delay:1.0,
+	scale:1.5,
+	ease:RoughEase.ease
+});
+
+TweenMax.to(".glow4", 5, {
+	delay:1.1,
+	scale:1.5,
+	ease:RoughEase.ease
+});
+
+TweenMax.to(".glow5", 5, {
+	delay:1.0,
+	scale:1.5,
+	ease:RoughEase.ease
+});
+
+TweenMax.to(".glow6", 5, {
+	delay:1.1,
+	scale:1.5,
+	ease:RoughEase.ease
+});
+
+TweenMax.to(".lights1", 2, {
+	delay:2,
+	x:180
+})
+
+TweenMax.to(".lights2", 2, {
+	delay:2.5,
+	x:210
+})
+
+TweenMax.to(".lights3", 2, {
+	delay:3.0,
+	x:220
+})
+
+TweenMax.to(".star1", 1, {
+	delay:4.2,
+	x:235
+})
+
+TweenMax.to(".sleigh", 9, {
+	delay:5.5,
+	x:1900,
+	y:-400,
+	scale: 0.4
+})
